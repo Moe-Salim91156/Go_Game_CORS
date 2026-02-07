e845b310cd51d89aca84a0c73db7c95edb78c0f7
Moe-Salim91156 <hmodaisleem88@gmail.com>
2026-02-07 19:23:29 +0300

    DB connection working fine

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	dbStore "CorsGame/internal/store/sqlite"
+	"log"
+)
+
 func main() {
+	db, err := dbStore.OpenConnection()
+	if err != nil {
+		log.Fatal("could not connect to DB", err)
+	}
+	defer db.Close()
+
+	err = dbStore.CreateTables(db)
+	if err != nil {
+		log.Fatal("could not create tables", err)
+	}
+
 }
--- backend/internal/store/sqlite/Db.go
+++ backend/internal/store/sqlite/Db.go
@@ -9,9 +9,8 @@ import (
 func OpenConnection() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", "./app.db")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer db.Close()
 
 	return db, nil
 }
@@ -22,7 +21,7 @@ func CreateTables(db *sql.DB) error {
 	UserTableQuery := `CREATE TABLE IF NOT EXISTS Players (
 	id  INTEGER PRIMARY KEY AUTOINCREMENT,
 	username TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL,
+	password TEXT NOT NULL
 );`
 
 	GameTableQuery := `CREATE TABLE IF NOT EXISTS GameRooms (
@@ -33,9 +32,9 @@ board TEXT DEFAULT '---------',
 turn_id INTEGER,
 game_state TEXT DEFAULT 'waiting',
 winner_id INTEGER,
-FOREIGN KEY(player_x_id) REFERENCES users(id),
-FOREIGN KEY(player_o_id) REFERENCES users(id),
-FOREIGN KEY(turn_id) REFERENCES users(id)
+FOREIGN KEY(player_x_id) REFERENCES players(id),
+FOREIGN KEY(player_o_id) REFERENCES players(id),
+FOREIGN KEY(turn_id) REFERENCES players(id)
 );`
 
 	_, err := db.Exec(UserTableQuery)
